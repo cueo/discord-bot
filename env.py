6ad63e414b8e884df97d8dc5dd4b113e61c87c3a
cueo <mohit.ritanil@gmail.com>
2021-04-06 18:21:02 +0530

    Load env variables only if file exists

--- env.py
+++ env.py
@@ -2,7 +2,8 @@ import os
 
 
 def load(file='.env'):
-    with open(file, 'r') as f:
-        for line in f.readlines():
-            key, value = line.split('=')
-            os.environ[key.strip()] = value.strip()
+    if os.path.exists(file):
+        with open(file, 'r') as f:
+            for line in f.readlines():
+                key, value = line.split('=')
+                os.environ[key.strip()] = value.strip()
