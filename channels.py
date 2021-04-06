b85ae3bb4cb3c3aa62040a81183f8a86ea096f84
cueo <mohit.ritanil@gmail.com>
2021-04-06 18:14:57 +0530

    Add constant file for channel names

---
+++ channels.py
@@ -0,0 +1,45 @@
+STATES = {
+    'Andhra Pradesh',
+    'Arunachal Pradesh',
+    'Assam',
+    'Bihar',
+    'Chhattisgarh',
+    'Goa',
+    'Gujarat',
+    'Haryana',
+    'Himachal Pradesh',
+    'Jharkhand',
+    'Karnataka',
+    'Kerala',
+    'Madhya Pradesh',
+    'Maharashtra',
+    'Manipur',
+    'Meghalaya',
+    'Mizoram',
+    'Nagaland',
+    'Odisha',
+    'Punjab',
+    'Rajasthan',
+    'Sikkim',
+    'Tamil Nadu',
+    'Telangana',
+    'Tripura',
+    'Uttar Pradesh',
+    'Uttarakhand',
+    'West Bengal',
+}
+STATES = {state.lower() for state in STATES}
+
+PLACES = {
+    'karnataka',
+    'tamil-nadu',
+    'delhi',
+    'gujarat',
+    'hyderabad',
+    'maharashtra',
+}
+
+STREAMS = {
+    'computer-science',
+    'information-security'
+}
